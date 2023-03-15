5161c8fdfe612276e8d31c62afe43f95826c0ebf
IrisAviva <manucadavidcorrea@gmail.com>
2023-03-15 16:03:11 -0500

    modificacion html

--- Script/Script.js
+++ Script/Script.js
@@ -17,5 +17,12 @@ console.log(nombre)
 function ValidarDatos(nombre, correo, mensaje){
    if(nombre.length==0 || correo.length==0 || mensaje.length==0) {
  
+    Swal.fire({
+        title: 'Error1',
+        text: 'do you want to continue',
+        icon: 'error',
+        confirmButtonText: 'cool',
+        iconColor: 'purple'
+    })
    }
 }
