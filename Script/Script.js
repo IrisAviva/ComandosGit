7e7c89519fa777d8683ca25a53cb96444c8a7a12
IrisAviva <manucadavidcorrea@gmail.com>
2023-03-15 15:56:26 -0500

    modificacion html

--- Script/Script.js
+++ Script/Script.js
@@ -3,7 +3,7 @@ let nombre, correo, memsaje;
 let formulario = document.getElementById('form');
 
 formulario.addEventListener('submit',(e)=>{
-    console.log('hola');
+
     e.preventDefault();
     LeerDatos()
 })
@@ -11,8 +11,8 @@ formulario.addEventListener('submit',(e)=>{
 function LeerDatos() {
  nombre= document.getElementById('nombre').value
  correo= document.getElementById('correo').value
- mensaje= document.querySelector('textarea').value
-alert(mensaje)
+ mensaje= document.querySelector('#textarea').value
+console.log(nombre)
 }
 function ValidarDatos(nombre, correo, mensaje){
    if(nombre.length==0 || correo.length==0 || mensaje.length==0) {
